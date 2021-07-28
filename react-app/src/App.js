59449d9079e46bf95759cc329dbb106dcc21ca12
Anthony <anthony.a.s.martin@gmail.com>
2021-07-28 13:32:57 -0400

    add invoice routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -28,20 +28,38 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
+        <Route path='/' exact={true}>
+            <h1> My Home Page</h1>
+        </Route>
+        <Route path='/features' exact={true}>
+            <h1> My Features Page</h1>
+        </Route>
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
         </Route>
+        <Route path='/page-not-found'>
+          <h1>Page Not Found</h1>
+        </Route>
+        <ProtectedRoute path='/invoices' exact={true} >
+          <h1>My ALL Invoice page</h1>
+        </ProtectedRoute>
+        <ProtectedRoute path='/invoices/:id' exact={true} >
+          <h1>Individual Invoice Page</h1>
+        </ProtectedRoute>
+        <ProtectedRoute path='/invoices/:id/payments' exact={true} >
+          <h1>Add a payment to invoice</h1>
+        </ProtectedRoute>
+        <ProtectedRoute path='/new-invoice' exact={true} >
+          <h1>My create an invoice page</h1>
+        </ProtectedRoute>
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
