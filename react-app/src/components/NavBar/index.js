28a89304d8ceb26e702585fd0ce9ef4ed30fda7f
Anthony <anthony.a.s.martin@gmail.com>
2021-07-27 23:52:40 -0400

    make changes to the navbar

---
+++ react-app/src/components/NavBar/index.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+import './NavBar.css'
+
+
+export default function NavBar(){
+  return (
+    <nav>
+      <ul className= 'nav-links-container'>
+        <li className='nav-links-left'>
+          <NavLink to='/' exact={true} activeClassName='active'>
+            <img src='logo.png' alt='yome'></img>
+          </NavLink>
+        </li>
+        <div className='nav-links-right'>
+          <li>
+            <NavLink to='/feature' exact={true} activeClassName='active'>
+              Feature
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+
