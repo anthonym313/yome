0bd09d41050a865cf22e643ef4490e666bdf5790
Anthony <anthony.a.s.martin@gmail.com>
2021-07-27 22:51:38 -0400

    add logo to nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            <img src='logo.png'></img>
           </NavLink>
         </li>
         <li>
