69b786102aa4f8a0402a44a541a760dd7c88a91a
Anthony <anthony.a.s.martin@gmail.com>
2021-07-29 18:39:27 -0400

    create clientcreation thunk

--- react-app/src/store/clients.js
+++ react-app/src/store/clients.js
@@ -1,9 +1,15 @@
 const GET_CLIENTS = 'clients/GET_CLIENTS';
+const SET_CLIENT = 'clients/SET_CLIENT';
 
 const getClients = (clientList)=>({
     type: GET_CLIENTS,
     clientList
 });
+
+const setClient = (client)=>({
+    type:SET_CLIENT,
+    client:client
+})
 
 export const getAllClients = () => async (dispatch) =>{
     const res = await fetch('/api/clients/');
@@ -12,6 +18,34 @@ export const getAllClients = () => async (dispatch) =>{
         dispatch(getClients(allClients));
     }
 };
+
+export const clientCreation = (name, email, streetaddress,phone)=> async (dispatch)=>{
+    const res = await fetch('/api/clients/new-client', {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            streetaddress,
+            phone
+        })
+    });
+
+    if (res.ok){
+        const data = await res.json();
+        dispatch(setClient(data))
+        return null;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+          return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
 
 const initialState = {}
 const clientReducer = (state = initialState, action)=>{
