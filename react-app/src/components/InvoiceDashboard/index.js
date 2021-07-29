bec852d54fda5812e0c0cb957e3f667c43daf7fe
Anthony <anthony.a.s.martin@gmail.com>
2021-07-28 21:49:53 -0400

    start invoice dashboard

---
+++ react-app/src/components/InvoiceDashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './InvoiceDashboard.css'
+
+export default function InvoiceDashboard(){
+    return(
+        <div className='invoice-dashboard-container'>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
