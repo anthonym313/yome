4b8380e63ebcb019ed328552b5c3ab41524df219
Anthony <anthony.a.s.martin@gmail.com>
2021-07-28 23:10:41 -0400

    debbugging invoice get request

--- app/api/invoice_routes.py
+++ app/api/invoice_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.models import db,Invoice,
+from app.models import db,Invoice
 from flask_login import current_user, login_required
 
 invoice_routes = Blueprint('invoices', __name__)
@@ -7,5 +7,5 @@ invoice_routes = Blueprint('invoices', __name__)
 @invoice_routes.route('/')
 @login_required
 def get_all_invoices():
-    invoices = Invoice.query.filter((current_user.id == business_id)).all()
-    return jsonify([invoice.to_dict() for invoice in invoices])
+    invoices = Invoice.query.filter((current_user.id == Invoice.business_id)).all()
+    return {'invoices':[invoice.to_dict() for invoice in invoices]}
