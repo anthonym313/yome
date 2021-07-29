2423a36693dfb2b23046114ed6b1f6bd54b9f196
Anthony <anthony.a.s.martin@gmail.com>
2021-07-28 23:10:06 -0400

    import invoice images

--- react-app/src/components/InvoiceDashboard/index.js
+++ react-app/src/components/InvoiceDashboard/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import { getAllInvoices } from '../../store/invoices';
 
 import './InvoiceDashboard.css'
 
 export default function InvoiceDashboard(){
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getAllInvoices())
+    },[dispatch])
     return(
         <div className='invoice-dashboard-container'>
             <div></div>
