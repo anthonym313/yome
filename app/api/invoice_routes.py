cd346d3afec5f5c619c96165d3b09273b7947a84
Anthony <anthony.a.s.martin@gmail.com>
2021-07-28 22:46:29 -0400

    create api route for all invoices dashboard

---
+++ app/api/invoice_routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, request, jsonify
+from app.models import db,Invoice,
+from flask_login import current_user, login_required
+
+invoice_routes = Blueprint('invoices', __name__)
+
+@invoice_routes.route('/')
+@login_required
+def get_all_invoices():
+    invoices = Invoice.query.filter((current_user.id == business_id)).all()
+    return jsonify([invoice.to_dict() for invoice in invoices])
