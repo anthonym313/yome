cb3e83ebe276c4aed19616870c7ad4f769dbc47d
Anthony <anthony.a.s.martin@gmail.com>
2021-07-29 18:09:26 -0400

    create flask form for client creation

---
+++ app/forms/client_creation_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class ClientCreationForm(FlaskForm):
+    name = StringField('name',validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    street_address = StringField('street_address', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
+    
--- react-app/src/components/ClientCreator/index.js
+++ react-app/src/components/ClientCreator/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import './ClientCreator.css'
 
 export default function ClientCreator(){
+
+    
     return(
         <div className='client-creationPage-container'>
             hi
