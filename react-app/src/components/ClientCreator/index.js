313f29f79a51502278b584bc36e3109fb1a10c27
Anthony <anthony.a.s.martin@gmail.com>
2021-07-29 17:50:02 -0400

    create new-client protected route

--- react-app/src/components/ClientCreator/index.js
+++ react-app/src/components/ClientCreator/index.js
@@ -5,7 +5,7 @@ import './ClientCreator.css'
 export default function ClientCreator(){
     return(
         <div className='client-creationPage-container'>
-            
+            hi
 
         </div>
     )
