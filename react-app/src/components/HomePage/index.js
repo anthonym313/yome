184acd3719c33426d39c07109ff32ded6992f608
Anthony <anthony.a.s.martin@gmail.com>
2021-08-01 15:23:48 -0400

    add hero image

---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './HomePage.css';
+
+export default function HomePage(){
+    
+    
+    
+    return(
+        <div className='home-container'>
+            <div className='hero-container'>
+
+            </div>
+            <div className='home-break-container'>
+                
+            </div>
+            
+            <div className='footeer'>
+
+            </div>
+
+        </div>
+    )
+}
