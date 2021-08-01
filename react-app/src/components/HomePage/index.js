e3e9b10584d101aca24e880df55c11532a5f862d
Anthony <anthony.a.s.martin@gmail.com>
2021-08-01 16:30:38 -0400

    add styling to hero container

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -9,10 +9,17 @@ export default function HomePage(){
     return(
         <div className='home-container'>
             <div className='hero-container'>
+                <img src='https://github.com/anthonym313/yome/blob/main/react-app/src/images/hero.jpg?raw=true' alt='invoice vector'></img>
+                <div className="hero-right-col">
+                    <h1>Invoice your customers</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button>Create An Invoice Now</button>
+                </div>
+                
 
             </div>
             <div className='home-break-container'>
-                
+
             </div>
             
             <div className='footeer'>
