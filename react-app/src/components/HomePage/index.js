03c4f24fc4392971c8a6c442274f4f3b8c2bbb41
Anthony <anthony.a.s.martin@gmail.com>
2021-08-01 17:25:30 -0400

    add content to hero of site

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -11,16 +11,19 @@ export default function HomePage(){
             <div className='hero-container'>
                 <img src='https://github.com/anthonym313/yome/blob/main/react-app/src/images/hero.jpg?raw=true' alt='invoice vector'></img>
                 <div className="hero-right-col">
-                    <h1>Invoice your customers</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className='hero-right-col-text'>
+                        <h1>Invoice your customers</h1>
+                        <h2>Get paid. Save Time. Look Professional</h2>
+                        <h4>Yome invoicing software helps get you the money that you are owed.</h4>
+                    </div>
                     <button>Create An Invoice Now</button>
                 </div>
+            </div>
+            <div className='home-break-container'>
                 
 
             </div>
-            <div className='home-break-container'>
 
-            </div>
             
             <div className='footeer'>
 
