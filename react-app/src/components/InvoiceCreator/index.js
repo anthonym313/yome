2ba343f5998e1f29e07e25b21f7853a5076e4844
Anthony <anthony.a.s.martin@gmail.com>
2021-08-04 13:21:17 -0400

    add background styling on creation page

--- react-app/src/components/InvoiceCreator/index.js
+++ react-app/src/components/InvoiceCreator/index.js
@@ -76,83 +76,86 @@ export default function InvoiceCreator(){
     
     
     return allClients &&(
-        <div className='invoice-creator-container'>
-            <div className='invoice-header'>
-                <img src={currentUser.logo_url} alt='user logo'></img>
-                <div>
-                    <h1>Invoice</h1>
-                </div>
-            </div>
+        <div className='iC-page'>
 
-               
-            <form  id='invoice-creator-form'>
-                <div className='invoiceCreator-errors'>
-                     {errors.map((error,ind)=>(
-                         <div key={ind}>{error}</div>
-                         ))}
-                </div>
-                <div className='invoice-top-inputs'>
+            <div className='invoice-creator-container'>
+                <div className='invoice-header'>
+                    <img src={currentUser.logo_url} alt='user logo'></img>
                     <div>
-                        <input 
-                        type='text'
-                        name='invoicenumber'
-                        placeholder='Invoice Number/ID'
-                        onChange={(e)=>setInvoiceNumber(e.target.value)}
-                        value={invoicenumber}
-                        required={true}
-                        ></input>
+                        <h1>New Invoice</h1>
                     </div>
-                    <div>
-                        <input
-                        type='date'
-                        onChange={(e)=> setDate(e.target.value)}
-                        value={date}
-                        required={true}
-                        ></input>
-                    </div>
-
                 </div>
-                <div className='busCli-container'>
-                    <div className='invoice-client-Info-container'>
-                        <label for='clients'> Choose a client</label>
-                        <select value={client} onChange={clientTings} id='client-drop'>
-                            {allClients.map((person,ind)=>(
-                                <option value={person.id} key={ind}>{person.name}</option>
-                                ))}
-                        </select>
-                    </div>
-                    <div className='invoice-business-Info-container'>
-                        <h4>{currentUser.username}</h4>
-                        <h5>{currentUser.street_address}</h5>
-                        <h5>{currentUser.city_state} {currentUser.zipcode}</h5>
-                        <h5>{currentUser.business_phone}</h5>
-                        <h5>{currentUser.phone}</h5>
-                    </div>
 
-                </div>
-                <thead>
-                    <th id='itemDes'>Description</th>
-                    <th id='itemRate'>Rate</th>
-                    <th id='itemQty'>Qty</th>
-                    <th>Amount</th>
-                </thead>
-                    {items.map(item =>(
-                        <div className='inv-item'>{item}</div>
+                
+                <form  id='invoice-creator-form'>
+                    <div className='invoiceCreator-errors'>
+                        {errors.map((error,ind)=>(
+                            <div key={ind}>{error}</div>
+                            ))}
+                    </div>
+                    <div className='invoice-top-inputs'>
+                        <div>
+                            <input 
+                            type='text'
+                            name='invoicenumber'
+                            placeholder='Invoice Number/ID'
+                            onChange={(e)=>setInvoiceNumber(e.target.value)}
+                            value={invoicenumber}
+                            required={true}
+                            ></input>
+                        </div>
+                        <div>
+                            <input
+                            type='date'
+                            onChange={(e)=> setDate(e.target.value)}
+                            value={date}
+                            required={true}
+                            ></input>
+                        </div>
+
+                    </div>
+                    <div className='busCli-container'>
+                        <div className='invoice-client-Info-container'>
+                            <label for='clients'> Choose a client</label>
+                            <select value={client} onChange={clientTings} id='client-drop'>
+                                {allClients.map((person,ind)=>(
+                                    <option value={person.id} key={ind}>{person.name}</option>
+                                    ))}
+                            </select>
+                        </div>
+                        <div className='invoice-business-Info-container'>
+                            <h4>{currentUser.username}</h4>
+                            <h5>{currentUser.street_address}</h5>
+                            <h5>{currentUser.city_state} {currentUser.zipcode}</h5>
+                            <h5>{currentUser.business_phone}</h5>
+                            <h5>{currentUser.phone}</h5>
+                        </div>
+
+                    </div>
+                    <thead>
+                        <th id='itemDes'>Description</th>
+                        <th id='itemRate'>Rate</th>
+                        <th id='itemQty'>Qty</th>
+                        <th>Amount</th>
+                    </thead>
+                        {items.map(item =>(
+                            <div className='inv-item'>{item}</div>
+                            
+                        ))}
+                    <div className='itm-addDel-buttons'>
+                        <button id='addItem-button' onClick={addItem}>Add Item</button>
+                        <button onClick={deleteItem}>Delete Item</button>
+                    </div>
                         
-                    ))}
-                <div className='itm-addDel-buttons'>
-                    <button id='addItem-button' onClick={addItem}>Add Item</button>
-                    <button onClick={deleteItem}>Delete Item</button>
-                </div>
-                    
 
-            </form>
-            <div className='invoice-bottom-container'>
-                <thead><th>Total Balance</th></thead>
-                <tbody><td>${Number.parseFloat(balance).toFixed(2)}</td></tbody>
-                <button type="submit" onClick={submitInvoiceHandler}>Create Invoice</button>
-            </div>    
-            
+                </form>
+                <div className='invoice-bottom-container'>
+                    <thead><th>Total Balance</th></thead>
+                    <tbody><td>${Number.parseFloat(balance).toFixed(2)}</td></tbody>
+                    <button type="submit" onClick={submitInvoiceHandler}>Create Invoice</button>
+                </div>    
+                
+            </div>
         </div>
                 
     )
