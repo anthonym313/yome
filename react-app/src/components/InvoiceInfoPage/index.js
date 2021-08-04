27e52fe4916383e7e248cdb982637273bafcd34f
Anthony <anthony.a.s.martin@gmail.com>
2021-08-04 19:18:41 -0400

    finish styling invoice preview

--- react-app/src/components/InvoiceInfoPage/index.js
+++ react-app/src/components/InvoiceInfoPage/index.js
@@ -13,7 +13,7 @@ export default function InvoiceInfoPage(){
     const currentInvoice = invoice[0] || null
     const [editMode, setEditMode] = useState(false)
     const headers = ['#', 'DESCRIPTION', 'RATE', 'QUANTITY', 'SUBTOTAL']
-    const headTotal=(anInvoice)=> (['','','','TOTAL', anInvoice.balance])
+    const headTotal=(anInvoice)=> (['','','','TOTAL', `$ ${Number.parseFloat(anInvoice.balance).toFixed(2)}`])
 
     useEffect(()=>{
         dispatch(getOneInvoice(invoice_number))
@@ -48,39 +48,40 @@ export default function InvoiceInfoPage(){
                     <div className='invoice-preview'>
 
                         <div className='inv-head'>
-                            <img src={currentUser.logo_url} alt='user logo'></img>
-                            <div>
-                                <h1>Invoice</h1>
+                            <div className='inv-head-logo'>
+                                <h1>INVOICE</h1>
+                                <img src={currentUser.logo_url} alt='user logo'></img>
                             </div>
                             <div>
-                                <h2>{currentInvoice.invoice_number}</h2>
+                                <h3>Invoice No.</h3>
+                                <p>{currentInvoice.invoice_number}</p>
                             </div>
                         </div>
                         <div className='client-company-infoContainer'>
                             <div className='client-information-container'>
-                                <h4>Customer Info</h4>
-                                <h5>{currentInvoice.clients.name}</h5>
-                                <h5>{currentInvoice.clients.street_address}</h5>
-                                <h5>{currentInvoice.clients.phone}</h5>
-                                <h5>{currentInvoice.clients.email}</h5>
+                                <h3>Customer Info</h3>
+                                <p>{currentInvoice.clients.name}</p>
+                                <p>{currentInvoice.clients.street_address}</p>
+                                <p>{currentInvoice.clients.phone}</p>
+                                <p>{currentInvoice.clients.email}</p>
                             </div>
                             <div className='company-info-container'>
-                                <h4>Company Info</h4>
-                                <h5>{currentUser.username}</h5>
-                                <h5>{currentUser.street_address}</h5>
-                                <h5>{currentUser.city_state}</h5>
-                                <h5>{currentUser.zipcode}</h5>
-                                <h5>{currentUser.business_phone}</h5>
-                                <h5>{currentUser.phone}</h5>
+                                <h3>Company Info</h3>
+                                <p>{currentUser.username}</p>
+                                <p>{currentUser.street_address}</p>
+                                <p>{currentUser.city_state} {currentUser.zipcode}</p>
+                                <p>{currentUser.email}</p>
+                                <p>{currentUser.business_phone}</p>
+                                <p>{currentUser.phone}</p>
                             </div>
                         </div>
                         <div id='order-date'>
-                            <h4>Order Date:</h4>
-                            <h5>{currentInvoice.date}</h5>
+                            <h3>Order Date:</h3>
+                            <p>{currentInvoice.date}</p>
                         </div>
                         <div>
                             <h2>Order Summary</h2>
-                            <table>
+                            <table id='invoice-summary'>
                                 <tbody>
                                     <tr>{tableHeaders(headers)}</tr>
                                     {tableData(currentInvoice.items)}
