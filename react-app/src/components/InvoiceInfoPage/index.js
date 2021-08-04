99797b9acc12b37607622d01930c054fb39deff0
Anthony <anthony.a.s.martin@gmail.com>
2021-08-04 13:48:31 -0400

    create invoice info page component

---
+++ react-app/src/components/InvoiceInfoPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function InvoiceInfoPage(){
+    return(
+        <div className='invoice-information-page-container'>
+            <div className='invoice-snapshot'>
+                <h1>Info page invoice</h1>
+            </div>
+
+        </div>
+    )
+
+}
