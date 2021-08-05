0f4af8b5477cc4cab6918e10b999230046d6a1f9
Anthony <anthony.a.s.martin@gmail.com>
2021-08-04 23:15:05 -0400

    add input to select new customer info;

--- react-app/src/components/InvoiceInfoPage/index.js
+++ react-app/src/components/InvoiceInfoPage/index.js
@@ -1,6 +1,7 @@
 import React,{useEffect,useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {useParams} from 'react-router-dom';
+import { getAllClients } from '../../store/clients';
 import { getOneInvoice } from '../../store/invoices';
 import './InvoiceInfoPage.css'
 
@@ -10,15 +11,27 @@ export default function InvoiceInfoPage(){
     const dispatch = useDispatch();
     const currentUser = useSelector((state)=>(state.session.user))
     const invoice = useSelector((state)=> Object.values(state.invoices))
+    const allClients = useSelector((state)=> Object.values(state.clients))
     const currentInvoice = invoice[0] || null
+    
     const [editMode, setEditMode] = useState(false)
+
+    const [invoicenumber, updateInvoiceNumber]= useState(currentInvoice?.invoice_number)
+    const [date, updateInvoiceDate]= useState(currentInvoice?.date)
+    const [clientid, updateInvoiceClientID]= useState(currentInvoice?.client_id)
+    
+
     const headers = ['#', 'DESCRIPTION', 'RATE', 'QUANTITY', 'SUBTOTAL']
     const headTotal=(anInvoice)=> (['','','','TOTAL', `$ ${Number.parseFloat(anInvoice.balance).toFixed(2)}`])
 
     useEffect(()=>{
+        dispatch(getAllClients())
         dispatch(getOneInvoice(invoice_number))
-    },[dispatch])
+    },[dispatch,invoice_number])
 
+    const clientSelectHandler = (e)=>{
+        updateInvoiceClientID(e.target.value)
+    }
     const tableHeaders = (array)=>{
         return array.map((head, index)=>{
             return <th key={index}>{head}</th>
@@ -53,39 +66,42 @@ export default function InvoiceInfoPage(){
                 </div>
                 <div className='invoice-information-page-container'>
                     <div className='invoice-preview'>
+                      
+                            <div className='inv-head'>
+                                <div className='inv-head-logo'>
+                                    <h1>INVOICE</h1>
+                                    <img src={currentUser.logo_url} alt='user logo'></img>
+                                </div>
+                                <div>
+                                    <h3>Invoice No.</h3>
+                                    <p>{currentInvoice.invoice_number}</p>
+                                </div>
+                            </div>
+                            <div className='client-company-infoContainer'>
+                                <div className='client-information-container'>
+                                    <h3>Customer Info</h3>
+                                    <p>{currentInvoice.clients.name}</p>
+                                    <p>{currentInvoice.clients.street_address}</p>
+                                    <p>{currentInvoice.clients.phone}</p>
+                                    <p>{currentInvoice.clients.email}</p>
+                                </div>
+                                <div className='company-info-container'>
+                                    <h3>Company Info</h3>
+                                    <p>{currentUser.username}</p>
+                                    <p>{currentUser.street_address}</p>
+                                    <p>{currentUser.city_state} {currentUser.zipcode}</p>
+                                    <p>{currentUser.email}</p>
+                                    <p>{currentUser.business_phone}</p>
+                                    <p>{currentUser.phone}</p>
+                                </div>
+                            </div>
+                            <div id='order-date'>
+                                <h3>Order Date:</h3>
+                                <p>{currentInvoice.date}</p>
+                            </div>
 
-                        <div className='inv-head'>
-                            <div className='inv-head-logo'>
-                                <h1>INVOICE</h1>
-                                <img src={currentUser.logo_url} alt='user logo'></img>
-                            </div>
-                            <div>
-                                <h3>Invoice No.</h3>
-                                <p>{currentInvoice.invoice_number}</p>
-                            </div>
-                        </div>
-                        <div className='client-company-infoContainer'>
-                            <div className='client-information-container'>
-                                <h3>Customer Info</h3>
-                                <p>{currentInvoice.clients.name}</p>
-                                <p>{currentInvoice.clients.street_address}</p>
-                                <p>{currentInvoice.clients.phone}</p>
-                                <p>{currentInvoice.clients.email}</p>
-                            </div>
-                            <div className='company-info-container'>
-                                <h3>Company Info</h3>
-                                <p>{currentUser.username}</p>
-                                <p>{currentUser.street_address}</p>
-                                <p>{currentUser.city_state} {currentUser.zipcode}</p>
-                                <p>{currentUser.email}</p>
-                                <p>{currentUser.business_phone}</p>
-                                <p>{currentUser.phone}</p>
-                            </div>
-                        </div>
-                        <div id='order-date'>
-                            <h3>Order Date:</h3>
-                            <p>{currentInvoice.date}</p>
-                        </div>
+                    
+
                         <div>
                             <h2>Order Summary</h2>
                             <table id='invoice-summary'>
@@ -102,7 +118,7 @@ export default function InvoiceInfoPage(){
             </div>
         )
     }else{
-        return currentInvoice && (
+        return currentInvoice && allClients && (
             <div className='individual-page-container-edit'>
                 <div className='preview-edit'>
                     <button onClick={handleClick}>Preview Mode</button>
@@ -111,39 +127,52 @@ export default function InvoiceInfoPage(){
                 </div>
                 <div className='invoice-information-page-container'>
                     <div className='invoice-preview'>
+                        <form>
 
-                        <div className='inv-head'>
-                            <div className='inv-head-logo'>
-                                <h1>INVOICE</h1>
-                                <img src={currentUser.logo_url} alt='user logo'></img>
+                            <div className='inv-head'>
+                                <div className='inv-head-logo'>
+                                    <h1>INVOICE</h1>
+                                    <img src={currentUser.logo_url} alt='user logo'></img>
+                                </div>
+                                <div>
+                                    <h3>Invoice No.</h3>
+                                    <input
+                                        type='text'
+                                        name='invoicenumber'
+                                        placeholder={currentInvoice.invoice_number}
+                                        onChange={(e)=>updateInvoiceNumber(e.target.value)}
+                                        value={invoicenumber}
+                                        required={true}
+
+                                        ></input>
+                                </div>
                             </div>
-                            <div>
-                                <h3>Invoice No.</h3>
-                                <p>{currentInvoice.invoice_number}</p>
+                            <div className='client-company-infoContainer'>
+                                <div className='client-information-container'>
+                                    <h3>Customer Info</h3>
+                                    <label htmlFor='clients'>Update Client</label>
+                                    <select value={clientid} onChange={clientSelectHandler}>
+                                        {allClients.map((person,ind)=>(
+                                            <option value={person.id} key={ind}>{person.name}</option>
+                                        ))}
+
+                                    </select>
+                                </div>
+                                <div className='company-info-container'>
+                                    <h3>Company Info</h3>
+                                    <p>{currentUser.username}</p>
+                                    <p>{currentUser.street_address}</p>
+                                    <p>{currentUser.city_state} {currentUser.zipcode}</p>
+                                    <p>{currentUser.email}</p>
+                                    <p>{currentUser.business_phone}</p>
+                                    <p>{currentUser.phone}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className='client-company-infoContainer'>
-                            <div className='client-information-container'>
-                                <h3>Customer Info</h3>
-                                <p>{currentInvoice.clients.name}</p>
-                                <p>{currentInvoice.clients.street_address}</p>
-                                <p>{currentInvoice.clients.phone}</p>
-                                <p>{currentInvoice.clients.email}</p>
+                            <div id='order-date'>
+                                <h3>Order Date:</h3>
+                                <p>{currentInvoice.date}</p>
                             </div>
-                            <div className='company-info-container'>
-                                <h3>Company Info</h3>
-                                <p>{currentUser.username}</p>
-                                <p>{currentUser.street_address}</p>
-                                <p>{currentUser.city_state} {currentUser.zipcode}</p>
-                                <p>{currentUser.email}</p>
-                                <p>{currentUser.business_phone}</p>
-                                <p>{currentUser.phone}</p>
-                            </div>
-                        </div>
-                        <div id='order-date'>
-                            <h3>Order Date:</h3>
-                            <p>{currentInvoice.date}</p>
-                        </div>
+                        </form>
                         <div>
                             <h2>Order Summary</h2>
                             <table id='invoice-summary'>
