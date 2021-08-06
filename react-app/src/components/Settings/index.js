3090e074384159c0ff067a895758e88847e50e48
Anthony <anthony.a.s.martin@gmail.com>
2021-08-06 17:55:42 -0400

    update company info complete without styling

--- react-app/src/components/Settings/index.js
+++ react-app/src/components/Settings/index.js
@@ -1,37 +1,146 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import {useSelector,useDispatch} from 'react-redux';
+
+import { editUser } from '../../store/session';
+import './Settings.css'
 
 export default function Settings() {
-  const [user, setUser] = useState({});
-  const { userId }  = useParams();
+  const user = useSelector((state)=>(state.session.user))
+  const dispatch= useDispatch()
+  const [id,setId]= useState(user.id)
+  const [username,updateUserName]=useState(user.username)
+  const [streetaddress,updateStreetAddress]=useState(user.street_address)
+  const [citystate,updateCityState]=useState(user.city_state)
+  const [zipcode,updateZipcode]=useState(user.zipcode)
+  const [phone,updatePhone]=useState(user.phone)
+  const [businessphone,updateBusinessPhone]=useState(user.business_phone)
+  const [logourl,updateLogoUrl]=useState(user.logo_url)
+
+
+
 
   useEffect(() => {
-    if (!userId) {
-      return;
-    }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
-  }, [userId]);
 
-  if (!user) {
-    return null;
+    setId(user.id)
+    
+  }, [user,username, streetaddress,citystate,zipcode,phone,businessphone,logourl]);
+  
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    dispatch(editUser(id, username, streetaddress,citystate,zipcode,phone,businessphone,logourl))
+    window.alert('Business Profile Settings Updated!')
   }
+  
+  return user && (
+    <div className="settings-page-container">
+      <div className='settings-card-container'>
+        <h2>My Settings</h2>
+        <div className="editable-user-settings">
+          <div className='profile-setting-header'>
+            <h3>Edit Business profile</h3>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className='user-logo-edits'>
+              <img src={user.logo_url} alt='business logo' style={{height:'150px'}}></img>
+              <div className='user-edit'>
+                <label>LogoUrl</label>
+                <input
+                type="text"
+                name="logo_url"
+                placeholder={logourl}
+                onChange={(e)=>updateLogoUrl(e.target.value)}
+                value={logourl}
+                required={true}
+                ></input>
+              </div>  
+            </div>
+              <div className='user-edit'>
+                <label>Business/ Company Name</label>
+                <input
+                type="text"
+                name="username"
+                placeholder={user.username}
+                onChange={(e)=>updateUserName(e.target.value)}
+                value={username}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>Street Address</label>
+                <input
+                type="text"
+                name="street_address"
+                placeholder={streetaddress}
+                onChange={(e)=>updateStreetAddress(e.target.value)}
+                value={streetaddress}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>City,State</label>
+                <input
+                type="text"
+                name="city_state"
+                placeholder={citystate}
+                onChange={(e)=>updateCityState(e.target.value)}
+                value={citystate}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>Zipcode</label>
+                <input
+                type="text"
+                name="zipcode"
+                placeholder={zipcode}
+                onChange={(e)=>updateZipcode(e.target.value)}
+                value={zipcode}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>Zipcode</label>
+                <input
+                type="text"
+                name="zipcode"
+                placeholder={zipcode}
+                onChange={(e)=>updateZipcode(e.target.value)}
+                value={zipcode}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>Business Phone Number</label>
+                <input
+                type="text"
+                name="business_phone"
+                placeholder={businessphone}
+                onChange={(e)=>updateBusinessPhone(e.target.value)}
+                value={businessphone}
+                required={true}
+                ></input>
+              </div>
+              <div className='user-edit'>
+                <label>Other Phone Number</label>
+                <input
+                type="text"
+                name="phone"
+                placeholder={phone}
+                onChange={(e)=>updatePhone(e.target.value)}
+                value={phone}
+                required={true}
+                ></input>
+              </div>
+            <button type='submit'>Save</button>
 
-  return (
-    <ul>
-      <li>
-        <strong>User Id</strong> {userId}
-      </li>
-      <li>
-        <strong>Username</strong> {user.username}
-      </li>
-      <li>
-        <strong>Email</strong> {user.email}
-      </li>
-    </ul>
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
   );
 }
+              
 
