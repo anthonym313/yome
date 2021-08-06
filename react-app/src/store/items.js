bf55b618c5d0724f241eb5eb7976f425be6d9d85
Anthony <anthony.a.s.martin@gmail.com>
2021-08-06 11:58:19 -0400

    start update and delete of items

--- react-app/src/store/items.js
+++ react-app/src/store/items.js
@@ -1,10 +1,16 @@
-// const SET_ITEM = 'items/SET_ITEM'
+const SET_ITEM = 'items/SET_ITEM';
+const REMOVE_ITEM = 'items/REMOVE_ITEM';
 
-// //Action Creators
-// const setItem = (item)=>({
-//     type:SET_ITEM,
-//     item:item
-// });
+//Action Creators
+const setItem = (item)=>({
+    type:SET_ITEM,
+    item:item
+});
+
+const removeItem =(item)=>({
+    type:REMOVE_ITEM,
+    item
+})
 
 //////////////Thunks/////
 //Create
@@ -31,14 +37,43 @@
 //     }
 // }
 
+//Update
+export const editItem = (id, description,rate,quantity,amount)=> async(dispatch)=>{
+    const res = await fetch(`/api/items/${id}/edit`,{
+        method:"PUT",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({id,description,rate,quantity,amount})
+    });
+    if(res.ok){
+        const updatedItem = await res.json();
+        dispatch(setItem(updatedItem));
+        return updatedItem;
+    }
+}
+//Delete
+export const deleteItem = (id) => async (dispatch) =>{
+    const res =await fetch (`/api/items/${id}`,{
+        method:'DELETE',
+        body:JSON.stringify({id}),
+    })
+    if(res.ok){
+        await res.json()
+        dispatch(removeItem(id));
+        return res;
+    }
+}
 
 //Reducer//
 const initialState={}
 const itemReducer = (state = initialState, action)=>{
-    
+    let newState = {}
     switch(action.type){
-        // case SET_ITEM:
-        //     return{item:action.item}
+        case SET_ITEM:
+            return {item:action.item}
+        case REMOVE_ITEM:
+            newState ={...state}
+            delete newState[action.id];
+            return {...newState}
         default:
             return state;
     }
